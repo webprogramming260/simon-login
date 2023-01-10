b191e5e2dfa63a90a9bf850d2928d54d3b9fb8da
Lee <lee.jensen@agilix.com>
2023-01-10 16:37:29 -0700

    add(ui) login, create

--- public/login.js
+++ public/login.js
@@ -14,16 +14,34 @@
   }
 })();
 
-async function login() {
-  const userNameEl = document.querySelector('#userName');
-  const user = await getUser(userNameEl.value);
-  if (user) {
-    console.log('login user');
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
+}
 
-    localStorage.setItem('userName', userNameEl.value);
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+  const userName = document.querySelector('#userName')?.value;
+  const password = document.querySelector('#userPassword')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const body = await response.json();
+
+  if (response?.status === 200) {
+    localStorage.setItem('userName', userName);
     window.location.href = 'play.html';
   } else {
-    console.log('create user');
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
   }
 }
 
