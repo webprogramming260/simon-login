f929c4ea27863afb5fb7223afaf43eddcec0eb92
Lee <lee.jensen@agilix.com>
2024-08-14 09:38:13 -0600

    add(login) checks for empty username or password

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Button from 'react-bootstrap/Button';
-import {MessageDialog} from './messageDialog';
+import { MessageDialog } from './messageDialog';
 
 export function Unauthenticated(props) {
   const [userName, setUserName] = React.useState(props.userName);
@@ -19,7 +19,7 @@ export function Unauthenticated(props) {
   async function loginOrCreate(endpoint) {
     const response = await fetch(endpoint, {
       method: 'post',
-      body: JSON.stringify({email: userName, password: password}),
+      body: JSON.stringify({ email: userName, password: password }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
@@ -38,27 +38,16 @@ export function Unauthenticated(props) {
       <div>
         <div className='input-group mb-3'>
           <span className='input-group-text'>@</span>
-          <input
-            className='form-control'
-            type='text'
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            placeholder='your@email.com'
-          />
+          <input className='form-control' type='text' value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='your@email.com' />
         </div>
         <div className='input-group mb-3'>
           <span className='input-group-text'>🔒</span>
-          <input
-            className='form-control'
-            type='password'
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder='password'
-          />
+          <input className='form-control' type='password' onChange={(e) => setPassword(e.target.value)} placeholder='password' />
         </div>
-        <Button variant='primary' onClick={() => loginUser()}>
+        <Button variant='primary' onClick={() => loginUser()} disabled={!userName || !password}>
           Login
         </Button>
-        <Button variant='secondary' onClick={() => createUser()}>
+        <Button variant='secondary' onClick={() => createUser()} disabled={!userName || !password}>
           Create
         </Button>
       </div>
